84fcfc405300230303cb4d8d49d49aecfd8c7c85
pradeepkrathnayaka <pradeepkrathnayaka@gmail.com>
2020-04-06 06:40:01 +0800

    Init hibernate spring boot demo project

---
+++ hibernate-springboot/src/main/java/com/example/demo/HBSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HBSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HBSpringbootApplication.class, args);
+	}
+
+}
