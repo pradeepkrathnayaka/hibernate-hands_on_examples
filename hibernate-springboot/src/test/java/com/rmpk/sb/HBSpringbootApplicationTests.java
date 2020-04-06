eafa007a7c55bf0fe1568f9805c38dd92c248d3d
pradeepkrathnayaka <pradeepkrathnayaka@gmail.com>
2020-04-06 08:04:31 +0800

    enhancement

--- hibernate-springboot/src/test/java/com/rmpk/sb/HBSpringbootApplicationTests.java
+++ hibernate-springboot/src/test/java/com/rmpk/sb/HBSpringbootApplicationTests.java
@@ -12,11 +12,8 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.annotation.Commit;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
